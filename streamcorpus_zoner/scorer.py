1708a8d4895256418a7429d421b5e6eeb24288a5
Dan Roberts <dan@diffeo.com>
2015-04-27 22:56:19 +0000

    Added a simple heuristic line based zoner method to the zoner class. The zoner class is configurable to allow for multiple zoners and will soon be extended to let you train a more complex model over data of interest.
    Also added scorer.py which lets you evaluate a zoner classification
    method and output precision, recall, and f-score.

---
+++ streamcorpus_zoner/scorer.py
@@ -0,0 +1,80 @@
+'''
+Script for scoring sample data using zoning classification. 
+'''
+from __future__ import division
+
+import argparse
+import yakonfig
+import dblogger
+
+from zoner import zoner
+
+def extract_labels(doc):
+    '''
+    to be run on labeled test data.
+    strips the first character from each line into
+    a list of labels. returns the label list and the 
+    raw data.
+    '''
+    labels = list()
+    data = list()
+    for line in doc:
+        ## coarse-grain over the given labels
+        if int(line[0]) == 2:
+            labels.append('body')
+        else:
+            labels.append('not_body')
+        data.append(line[2:])
+
+    return labels, data
+
+def score(zones, labels):
+    '''
+    Compare the classification output in `zones' to the ground truth
+    data in `labels'. Compute precision, recall, and f-score.
+    '''
+
+    assert len(zones) == len(labels)
+
+    TP = 0
+    FN = 0
+    TN = 0
+    FP = 0
+
+    for i in xrange(len(zones)):
+        zone = zones[i]
+        label = labels[i]
+        TP += label == zone and label == 'body'
+        TN += label == zone and label == 'not_body'
+        FP += not label == zone and label == 'not_body'
+        FN += not label == zone and label == 'body'
+
+    P = TP / (TP + FP)
+    R = TP / (TP + FN)
+    F = 2*P*R / (P + R)
+
+    return {'P': P, 'R': R, 'F': F}
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'example', 
+        help='an example file')
+    args = yakonfig.parse_args(parser, [yakonfig, dblogger])
+    example_file_name = args.example
+    example_doc = open(example_file_name, 'r')
+
+    ## separate the labels from the data
+    labels, data = extract_labels(example_doc)
+
+    ## classify example doc using simple zoner
+    zoner_simple = zoner('simple')
+    zones = zoner_simple.classify(data)
+    scores = score(zones, labels)
+
+    print 'Precision: %f, Recall: %f, F-score: %f' % \
+            (scores['P'], scores['R'], scores['F'])
+
--- streamcorpus_zoner/zoner.py
+++ streamcorpus_zoner/zoner.py
@@ -3,19 +3,47 @@
 '''
 
 
-from streamcorpus import OffsetType, Offset
+#from streamcorpus import OffsetType, Offset
+from features import fraction_stop_words_chars, fraction_punctuation
 
 
 class zoner(object):
 
-    def process_item(self, si, context=None):
 
-        tags = run_zoner(si.body.clean_visible)
-        for line_number, zone_number in tags:
-            off = Offset(
-                type=OffsetType.LINES,
-                first=tag,
-                length=1,
-                value=zone_number,
-            )
-            #si.body.zones
+    def __init__(self, classifier):
+        '''
+        picks which zoner to use
+        '''
+        available_classifiers = {'simple': self.classify_simple}
+        self.classify = available_classifiers[classifier]
+
+
+    # def process_item(self, si, context=None):
+
+    #     tags = run_zoner(si.body.clean_visible)
+    #     for line_number, zone_number in tags:
+    #         off = Offset(
+    #             type=OffsetType.LINES,
+    #             first=tag,
+    #             length=1,
+    #             value=zone_number,
+    #         )
+    #         #si.body.zones
+
+    def classify_line_simple(self, line):
+        '''
+        A heuristic for classifying lines
+        '''
+        if fraction_stop_words_chars(line) > fraction_punctuation(line):
+            return 'body'
+        else:
+            return 'not_body'
+
+    def classify_simple(self, doc):
+        '''
+        Uses the line based simple heuristic classifier to classify a document
+        '''
+        zones = list()
+        for line in doc:
+            zones.append(self.classify_line_simple(line))
+        return zones
